f0ce4eaf38e4313c16c922d1fd7aa461f6f287cf
Rick van Loon <rick@bumbal.eu>
2022-04-23 13:50:36 +0200

    Added in memory database for User CRUD

---
+++ src/database/db.js
@@ -0,0 +1,69 @@
+const userDB = [];
+const timeout = 500;
+let id = 0;
+
+module.exports = {
+    listUsers(callback) {
+        setTimeout(() => {
+            callback(userDB);
+        }, timeout);
+    },
+
+    createUser(user, callback) {
+        if (
+            !user ||
+            !user.emailAdress ||
+            userDB.filter((item) => item.emailAdress === user.emailAdress).length > 0
+        ) {
+            const error = 'A user with this email is already registered.';
+            callback(error, undefined);
+        } else {
+            id++
+            const newUser = {
+                ...user,
+                id,
+            }
+            userDB.push(newUser);
+            callback(undefined, newUser);
+        }
+    },
+
+    retrieveUser(id, callback) {
+        setTimeout(() => {
+            callback(userDB.find((user) => user.id == id) ?? {});
+        }, timeout);
+    },
+
+    updateUser(user, id, callback) {
+        setTimeout(() => {
+            const index = userDB.findIndex((item) => id == item.id);
+            if (
+                user.id ||
+                index === -1
+            ) {
+                const error = 'Invalid userID';
+                callback(error, undefined);
+            } else {
+                userDB[index] = {
+                    ...userDB[index],
+                    ...user
+                }
+                callback(undefined, userDB[index]);
+            }
+        }, timeout);
+    },
+
+    deleteUser(id, callback) {
+        setTimeout(() => {
+            const index = userDB.findIndex((item) => id == item.id);
+            if (
+                index === -1
+            ) {
+                const error = 'User not found';
+                callback(error, undefined);
+            } else {
+                callback(undefined, userDB.splice(index, 1)[0]);
+            }
+        }, timeout);
+    },
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,83 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const database = require('./database/db');
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/api/user', (req, res) => {
+    database.listUsers((result) => {
+        res.status(200).json({
+            statusCode: 200,
+            result
+        });
+    });
 });
 
-const PORT = process.env.PORT || 5000;
+app.get('/api/user/:id', (req, res) => {
+    database.retrieveUser(req.params.id, (result) => {
+        res.status(200).json({
+            statusCode: 200,
+            result
+        })
+    });
+});
+
+app.post('/api/user', (req, res) => {
+    database.createUser(req.body, (error, result) => {
+        if (error) {
+            res.status(400).json({
+                statusCode: 400,
+                error
+            });
+        } else {
+            res.status(200).json({
+                statusCode: 200,
+                result
+            });
+        }
+    })
+});
+
+app.put('/api/user/:id', (req, res) => {
+    database.updateUser(req.body, req.params.id, (error, result) => {
+        if (error) {
+            res.status(400).json({
+                statusCode: 400,
+                error
+            });
+        } else {
+            res.status(200).json({
+                statusCode: 200,
+                result
+            });
+        }
+    });
+});
+
+app.delete('/api/user/:id', (req, res) => {
+    database.deleteUser(req.params.id, (error, result) => {
+        if (error) {
+            res.status(400).json({
+                statusCode: 400,
+                error
+            });
+        } else {
+            res.status(200).json({
+                statusCode: 200,
+                result
+            });
+        }
+    });
+});
+
+app.use('*', (req, res) => {
+    res.status(401).json({
+        status: 401,
+        result: 'End-point not found',
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
