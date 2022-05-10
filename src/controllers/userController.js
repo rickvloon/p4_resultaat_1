6c7f3b664de3febcb3a0a3d91295f7b9322720eb
Rick van Loon <rick@bumbal.eu>
2022-05-10 23:08:21 +0200

    Message fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -300,6 +300,7 @@ module.exports = {
                     } else {
                         res.status(200).json({
                             statusCode: 200,
+                            message: 'User deleted'
                         });
                     }
                 }
