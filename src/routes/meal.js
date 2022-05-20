d843f2330f74305af0df2ea806f6b28e5826f14b
Rick van Loon <rick@bumbal.eu>
2022-05-20 11:58:07 +0200

    Added testcases

--- src/routes/meal.js
+++ src/routes/meal.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const mealController = require('../controllers/mealController');
 const authController = require('../controllers/authController');
 
-router.post('/', mealController.validateMeal, mealController.createMeal);
+router.post('/', mealController.validateMeal, authController.validateToken, mealController.createMeal);
 
 module.exports = router;
