a82f54897aab6e9f51f42471fce8e458bcc2ba33
Rick van Loon <rick@bumbal.eu>
2022-05-21 11:58:34 +0200

    Changed datetime value for updat meal

--- test/integration/meal.route.test.js
+++ test/integration/meal.route.test.js
@@ -350,7 +350,7 @@ describe('Manage meals /api/meal/', () => {
                     isVega: true,
                     isVegan: true,
                     isToTakeHome: true,
-                    dateTime: '2022-05-20T08:30:53.232Z',
+                    dateTime: '2022-05-12 10:15:11',
                     imageUrl:
                         'https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg',
                     allergenes: ['gluten', 'noten', 'lactose'],
@@ -378,7 +378,7 @@ describe('Manage meals /api/meal/', () => {
                         isVegan: true,
                         isToTakeHome: true,
                         maxAmountOfParticipants: 6,
-                        dateTime: '2022-05-20T06:30:53.000Z',
+                        dateTime: '2022-05-12 10:15:11',
                         price: 6.75,
                         imageUrl:
                             'https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg',
